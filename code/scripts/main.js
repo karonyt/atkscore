25cbea14c2f12c29463e35916b552ba1aef56b75
かろん <104133597+karonyt@users.noreply.github.com>
2023-02-13 16:34:34 +0900

    Update main.js

--- code/scripts/main.js
+++ code/scripts/main.js
@@ -3,11 +3,11 @@ import { getScore } from "getscore.js";
 
 world.events.entityHurt.subscribe(ev => {
   let d = 1
-  d = getScore(ev.damagingEntity, "atk" )
+  d = getScore(ev.damageSource.damagingEntity, "atk" )
   d = ( d - 1 ) * 2
   const def = ev.hurtEntity
-  const atk = ev.damagingEntity
-  def.runCommandAsync(`effect @s instant_health 0`)
-  def.runCommandAsync(`effect @s instant_damage 0`)
-  def.runCommandAsync(`damage @s ${d} entity_attack entity @e[c=1,name="${atk.nameTag}",type=${atk.typeId}]`)
+  const atk = ev.damageSource.damagingEntity
+  def?.runCommandAsync(`effect @s instant_health 0`)
+  def?.runCommandAsync(`effect @s instant_damage 0`)
+  def?.runCommandAsync(`damage @s ${d} entity_attack entity @e[c=1,name="${atk.nameTag}",type=${atk.typeId}]`)
 })
